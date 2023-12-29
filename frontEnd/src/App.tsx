ef23abdb7a13c9cf911b9739bcd153c7628a8c70
wlals <jiminelp@gmail.com>
2023-12-29 10:36:58 +0900

    URL 수정

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { io } from "socket.io-client";
 
 let interval = 3000;
+const serverURL = process.env.REACT_APP_SERVER_URL;
 export default function App() {
-  const onSocket = () => {
-    const socket = io("http://192.168.1.21:3010");
+  useEffect(() => {
+    const socket = io(`${serverURL}:3010`);
 
     setInterval(() => {
       socket.emit("good", "클라이언트 -> 서버");
     }, interval);
 
     socket.on("hi", (data) => console.log(data)); // 서버 -> 클라이언트
-  };
 
-  return <button onClick={onSocket}>socket 통신 시작</button>;
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return <button>socket 통신 시작</button>;
 }
